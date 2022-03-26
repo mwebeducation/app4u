31e69655c745f079847faae2f60b8a32917ffe3e
mwebeducation <mwebeducationchannel@gmail.com>
2022-03-26 15:36:35 +0630

    add error handling

--- server/app.js
+++ server/app.js
@@ -1,19 +1,66 @@
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const logger = require('morgan')
+const hpp = require('hpp')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const csurf = require('csurf')
+const mongoSanitize = require('express-mongo-sanitize')
+const compression = require('compression')
+const toobusy = require('toobusy-js')
+const createErr = require('http-errors')
 
 const app = express()
 
 /**
- *
- * @Main @Function
+ * @Configuration
  */
 
-const main = () => {
-  // testing
-  app.get('/', (req, res, next) => {
-    res.send('hello world')
-  })
+const corsOpt = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
 }
 
-main()
+const json = express.json
+const urlEncoded = express.urlencoded
+
+/**
+ * @Middlewares
+ */
+app.enable('trust proxy') // trust first proxy
+app.use(cors(corsOpt))
+app.use(logger('dev'))
+app.use(json())
+app.use(urlEncoded({ extended: true }))
+app.use(helmet())
+app.use(compression())
+app.use(hpp())
+app.use(mongoSanitize())
+
+/**
+ * @Error section
+ */
+app.use((_req, _res, next) => {
+  if (!toobusy()) return next()
+  return next(createErr(503))
+})
+
+app.use((req, res, next) => next(createErr(404)))
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  const message = err.message || 'Internal Server Error!'
+  res
+    .status(status)
+    .json({
+      errors: {
+        status,
+        message,
+      },
+    })
+    .end()
+  return next()
+})
 
 module.exports = app
